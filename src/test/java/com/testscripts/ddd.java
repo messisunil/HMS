fd5b871b1b09e8c3bea75af6d124c72c961bb25e
sunil <sunil@DESKTOP-D716IJ6>
2023-04-06 17:25:21 +0530

    msa

---
+++ src/test/java/com/testscripts/ddd.java
@@ -0,0 +1,17 @@
+package com.testscripts;
+
+import org.testng.annotations.Test;
+
+public class ddd {
+@Test
+public void m2()
+{
+	String URL = System.getProperty("url");
+	String BROWSER = System.getProperty("browser");
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	
+	
+	
+}
+}
